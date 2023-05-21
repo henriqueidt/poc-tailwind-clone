db5225813831bfc175d2c9d595c1e4495ecb8f1d
Henrique Eidt <henriqueidt@gmail.com>
2023-05-21 00:45:46 -0300

    remove webpack from project

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-const webpack = require("webpack");
-const WebpackDevServer = require("webpack-dev-server");
-const webpackConfig = require("./webpack.config.js");
 const myFunction = require("./src/utils/parser.ts");
 const fs = require("fs");
+import express from "express";
+
+const htmlUrl = "index.html";
 
 const html = fs.readFileSync("index.html", "utf8");
 
@@ -10,39 +10,16 @@ const selectors = myFunction(html);
 
 console.log("slectors", selectors);
 
-// Create a Webpack compiler instance with the provided configuration
-const compiler = webpack(webpackConfig);
+const app = express();
+const port = 3000;
 
-const devServerOptions = { ...webpackConfig.devServer, open: false };
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-// Create a new WebpackDevServer instance
-const server = new WebpackDevServer(devServerOptions, compiler);
-
-const runServer = async () => {
-  console.log("Starting server...");
-  await server.start();
-};
-
-runServer();
-
-// Run the compilation process
-// compiler.run((err, stats) => {
-//   console.log("HABDHASBDHABDHASDBAHDBAHSDBSAH");
-
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-
-//   // Process the build statistics and output relevant information
-//   console.log(
-//     stats.toString({
-//       colors: true,
-//       modules: false,
-//       chunks: false,
-//       chunkModules: false,
-//     })
-//   );
-
-//   console.log("asd");
-// });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  fs.watchFile(htmlUrl, (abc) => {
+    console.log(`${htmlUrl} file Changed`);
+  });
+});
